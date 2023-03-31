49b151cbf8c9760c8eed6672731ba45d6d59a68e
Timothy Sukamtoh <timtoh168@gmail.com>
2023-03-31 09:07:55 +0900

    basic game functions added

--- memory-game.js
+++ memory-game.js
@@ -9,7 +9,7 @@ const COLORS = [
 ];
 
 const colors = shuffle(COLORS);
-
+let secondCard = false;
 createCards(colors);
 
 
@@ -43,6 +43,13 @@ function createCards(colors) {
 
   for (let color of colors) {
     // missing code here ...
+    const card = document.createElement('button');
+    card.classList.add(color);
+    gameBoard.append(card);
+
+    card.addEventListener('click',(e)=>{
+      handleCardClick(e);
+    })
   }
 }
 
@@ -50,16 +57,67 @@ function createCards(colors) {
 
 function flipCard(card) {
   // ... you need to write this ...
+  card.classList.add('active');
 }
 
 /** Flip a card face-down. */
 
 function unFlipCard(card) {
   // ... you need to write this ...
+  card.classList.remove('active');
+}
+
+function setCard(card){
+  unFlipCard(card);
+  card.classList.add('set');
+}
+
+/** function to disable or enable card button */
+function enableDisable(card){
+  if(card.disabled = true){
+    card.disabled = false;
+  } else {
+    card.disabled = true;
+  }
 }
 
 /** Handle clicking on a card: this could be first-card or second-card. */
 
 function handleCardClick(evt) {
   // ... you need to write this ...
+  const gameBoard = document.getElementById("game");
+  
+  let activeChild;
+  for(const child of gameBoard.children){
+    if(child.classList.contains('active')){
+      secondCard = true;
+      activeChild = child;
+    }
+  }
+  if(!secondCard){
+    flipCard(evt.target);
+  } else {
+    flipCard(evt.target);
+    for(const child of gameBoard.children){
+      enableDisable(child);
+    }
+    let color1 = Array.from(activeChild.classList)[0]
+    let color2 = Array.from(evt.target.classList)[0]
+    console.log(color1,color2)
+    if(color1!==color2){
+      console.log('do timeout')
+      setTimeout(function(){
+        unFlipCard(evt.target);
+        unFlipCard(activeChild);  
+      }, 1500);
+    } else {
+      setCard(evt.target);
+      setCard(activeChild);
+
+    }
+    enableDisable(evt.target);
+    enableDisable(activeChild);
+    secondCard = false;
+  }
+  console.log(secondCard);
 }
